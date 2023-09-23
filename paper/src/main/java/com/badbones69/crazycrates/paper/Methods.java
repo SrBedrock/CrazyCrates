42d7f940eeeed307e8e0029843be903a0afa3ae6
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2023-09-23 16:30:41 -0300

    better hex pattern

--- paper/src/main/java/com/badbones69/crazycrates/paper/Methods.java
+++ paper/src/main/java/com/badbones69/crazycrates/paper/Methods.java
@@ -36,14 +36,17 @@ public class Methods {
 
     private static final CrazyManager crazyManager = plugin.getStarter().getCrazyManager();
 
-    public final static Pattern HEX_PATTERN = Pattern.compile("#[a-fA-F\\d]{6}");
+    private static final Pattern HEX_PATTERN = Pattern.compile("&?#[a-f\\d]{6}", Pattern.CASE_INSENSITIVE);
 
     public static String color(String message) {
         Matcher matcher = HEX_PATTERN.matcher(message);
         StringBuilder buffer = new StringBuilder();
 
         while (matcher.find()) {
-            matcher.appendReplacement(buffer, net.md_5.bungee.api.ChatColor.of(matcher.group()).toString());
+            String colorCode = matcher.group();
+            colorCode = colorCode.replace("&", "");
+            String replacement = net.md_5.bungee.api.ChatColor.of(colorCode).toString();
+            matcher.appendReplacement(buffer, replacement);
         }
 
         return ChatColor.translateAlternateColorCodes('&', matcher.appendTail(buffer).toString());
