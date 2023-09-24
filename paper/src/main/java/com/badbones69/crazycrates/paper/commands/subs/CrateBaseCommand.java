d3a9d3c099483c70169acd03515067e17d27e25b
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2023-09-24 19:03:55 -0300

    remove crate transfer

--- paper/src/main/java/com/badbones69/crazycrates/paper/commands/subs/CrateBaseCommand.java
+++ paper/src/main/java/com/badbones69/crazycrates/paper/commands/subs/CrateBaseCommand.java
@@ -61,50 +61,6 @@ public class CrateBaseCommand extends BaseCommand {
         sender.sendMessage(Messages.HELP.getMessage());
     }
 
-    @SubCommand("transfer")
-    @Permission(value = "crazycrates.command.player.transfer", def = PermissionDefault.OP)
-    public void onPlayerTransferKeys(Player sender, @Suggestion("crates") String crateName, @Suggestion("online-players") Player player, @Suggestion("numbers") int amount) {
-        Crate crate = crazyManager.getCrateFromName(crateName);
-
-        if (crate != null) {
-            if (!player.getName().equalsIgnoreCase(sender.getName())) {
-
-                if (crazyManager.getVirtualKeys(sender, crate) >= amount) {
-                    PlayerReceiveKeyEvent event = new PlayerReceiveKeyEvent(player, crate, PlayerReceiveKeyEvent.KeyReceiveReason.TRANSFER, amount);
-                    plugin.getServer().getPluginManager().callEvent(event);
-
-                    if (!event.isCancelled()) {
-                        crazyManager.takeKeys(amount, sender, crate, KeyType.VIRTUAL_KEY, false);
-                        crazyManager.addKeys(amount, player, crate, KeyType.VIRTUAL_KEY);
-
-                        HashMap<String, String> placeholders = new HashMap<>();
-
-                        placeholders.put("%Crate%", crate.getName());
-                        placeholders.put("%Amount%", amount + "");
-                        placeholders.put("%Player%", player.getName());
-
-                        sender.sendMessage(Messages.TRANSFERRED_KEYS.getMessage(placeholders));
-
-                        placeholders.put("%Player%", sender.getName());
-
-                        player.sendMessage(Messages.RECEIVED_TRANSFERRED_KEYS.getMessage(placeholders));
-
-                        boolean logFile = FileManager.Files.CONFIG.getFile().getBoolean("Settings.Crate-Actions.Log-File");
-                        boolean logConsole = FileManager.Files.CONFIG.getFile().getBoolean("Settings.Crate-Actions.Log-Console");
-
-                        eventLogger.logKeyEvent(player, sender, crate, KeyType.VIRTUAL_KEY, EventLogger.KeyEventType.KEY_EVENT_RECEIVED, logFile, logConsole);
-                    }
-                } else {
-                    sender.sendMessage(Messages.NOT_ENOUGH_KEYS.getMessage("%Crate%", crate.getName()));
-                }
-            } else {
-                sender.sendMessage(Messages.SAME_PLAYER.getMessage());
-            }
-        } else {
-            sender.sendMessage(Messages.NOT_A_CRATE.getMessage("%Crate%", crateName));
-        }
-    }
-
     @SubCommand("admin-help")
     @Permission(value = "crazycrates.command.admin.help", def = PermissionDefault.OP)
     public void onAdminHelp(CommandSender sender) {
