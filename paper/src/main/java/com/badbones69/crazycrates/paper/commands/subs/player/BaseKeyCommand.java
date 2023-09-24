6df6b29ba2a84b35d2b352d8cc2d1e2a93a2b0b3
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2023-09-24 19:01:34 -0300

    add chave transferir

--- paper/src/main/java/com/badbones69/crazycrates/paper/commands/subs/player/BaseKeyCommand.java
+++ paper/src/main/java/com/badbones69/crazycrates/paper/commands/subs/player/BaseKeyCommand.java
@@ -1,24 +1,36 @@
 package com.badbones69.crazycrates.paper.commands.subs.player;
 
+import com.badbones69.crazycrates.api.enums.types.KeyType;
 import com.badbones69.crazycrates.paper.CrazyCrates;
 import com.badbones69.crazycrates.paper.api.CrazyManager;
+import com.badbones69.crazycrates.paper.api.EventLogger;
+import com.badbones69.crazycrates.paper.api.FileManager;
 import com.badbones69.crazycrates.paper.api.enums.settings.Messages;
+import com.badbones69.crazycrates.paper.api.events.PlayerReceiveKeyEvent;
 import com.badbones69.crazycrates.paper.api.objects.Crate;
-import com.google.common.collect.Lists;
 import dev.triumphteam.cmd.bukkit.annotation.Permission;
 import dev.triumphteam.cmd.core.BaseCommand;
-import dev.triumphteam.cmd.core.annotation.*;
+import dev.triumphteam.cmd.core.annotation.Command;
+import dev.triumphteam.cmd.core.annotation.Default;
+import dev.triumphteam.cmd.core.annotation.SubCommand;
+import dev.triumphteam.cmd.core.annotation.Suggestion;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionDefault;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@Command(value = "keys", alias = {"key"})
+@Command(value = "chaves", alias = {"chave"})
 public class BaseKeyCommand extends BaseCommand {
 
     private final CrazyCrates plugin = CrazyCrates.getPlugin();
 
     private final CrazyManager crazyManager = plugin.getStarter().getCrazyManager();
+
+    private final EventLogger eventLogger = plugin.getStarter().getEventLogger();
 
     @Default
     @Permission("crazycrates.command.player.key")
@@ -30,9 +42,53 @@ public class BaseKeyCommand extends BaseCommand {
         getKeys(player, player, header, noKeys);
     }
 
-    @SubCommand("view")
+    @SubCommand("transferir")
+    @Permission(value = "crazycrates.command.player.transfer", def = PermissionDefault.OP)
+    public void onPlayerTransferKeys(Player sender, @Suggestion("crates") String crateName, @Suggestion("online-players") Player player, @Suggestion("numbers") int amount) {
+        Crate crate = crazyManager.getCrateFromName(crateName);
+
+        if (crate != null) {
+            if (!player.getName().equalsIgnoreCase(sender.getName())) {
+
+                if (crazyManager.getVirtualKeys(sender, crate) >= amount) {
+                    PlayerReceiveKeyEvent event = new PlayerReceiveKeyEvent(player, crate, PlayerReceiveKeyEvent.KeyReceiveReason.TRANSFER, amount);
+                    plugin.getServer().getPluginManager().callEvent(event);
+
+                    if (!event.isCancelled()) {
+                        crazyManager.takeKeys(amount, sender, crate, KeyType.VIRTUAL_KEY, false);
+                        crazyManager.addKeys(amount, player, crate, KeyType.VIRTUAL_KEY);
+
+                        HashMap<String, String> placeholders = new HashMap<>();
+
+                        placeholders.put("%Crate%", crate.getName());
+                        placeholders.put("%Amount%", amount + "");
+                        placeholders.put("%Player%", player.getName());
+
+                        sender.sendMessage(Messages.TRANSFERRED_KEYS.getMessage(placeholders));
+
+                        placeholders.put("%Player%", sender.getName());
+
+                        player.sendMessage(Messages.RECEIVED_TRANSFERRED_KEYS.getMessage(placeholders));
+
+                        boolean logFile = FileManager.Files.CONFIG.getFile().getBoolean("Settings.Crate-Actions.Log-File");
+                        boolean logConsole = FileManager.Files.CONFIG.getFile().getBoolean("Settings.Crate-Actions.Log-Console");
+
+                        eventLogger.logKeyEvent(player, sender, crate, KeyType.VIRTUAL_KEY, EventLogger.KeyEventType.KEY_EVENT_RECEIVED, logFile, logConsole);
+                    }
+                } else {
+                    sender.sendMessage(Messages.NOT_ENOUGH_KEYS.getMessage("%Crate%", crate.getName()));
+                }
+            } else {
+                sender.sendMessage(Messages.SAME_PLAYER.getMessage());
+            }
+        } else {
+            sender.sendMessage(Messages.NOT_A_CRATE.getMessage("%Crate%", crateName));
+        }
+    }
+
+    @SubCommand("ver")
     @Permission("crazycrates.command.player.key.others")
-    public void viewOthers(CommandSender sender, @Suggestion ("online-players") Player target) {
+    public void viewOthers(CommandSender sender, @Suggestion("online-players") Player target) {
         if (target == sender) {
             sender.sendMessage(Messages.SAME_PLAYER.getMessage());
             return;
@@ -46,7 +102,7 @@ public class BaseKeyCommand extends BaseCommand {
     }
 
     private void getKeys(Player target, CommandSender sender, String header, String messageContent) {
-        List<String> message = Lists.newArrayList();
+        List<String> message = new ArrayList<>();
 
         message.add(header);
 
@@ -54,8 +110,9 @@ public class BaseKeyCommand extends BaseCommand {
 
         boolean hasKeys = false;
 
-        for (Crate crate : keys.keySet()) {
-            int amount = keys.get(crate);
+        for (Map.Entry<Crate, Integer> entry : keys.entrySet()) {
+            Crate crate = entry.getKey();
+            int amount = entry.getValue();
 
             if (amount > 0) {
                 hasKeys = true;
