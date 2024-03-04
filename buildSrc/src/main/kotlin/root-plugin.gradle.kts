8c88a8c2e16da504584cc5f15cc8a409091f230e
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2024-03-03 22:22:31 -0300

    Revertendo configurações

--- buildSrc/src/main/kotlin/root-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/root-plugin.gradle.kts
@@ -1,4 +1,5 @@
 import io.papermc.hangarpublishplugin.model.Platforms
+import org.gradle.kotlin.dsl.support.uppercaseFirstChar
 import java.io.ByteArrayOutputStream
 
 plugins {
@@ -23,6 +24,44 @@ repositories {
     mavenCentral()
 }
 
+// The commit id for the "main" branch prior to merging a pull request.
+val start = "8f6f4dd"
+
+// The commit id BEFORE merging the pull request so before "Merge pull request #30"
+val end = "8f6f4dd"
+
+val commitLog = getGitHistory().joinToString(separator = "") { formatGitLog(it) }
+
+fun getGitHistory(): List<String> {
+    val output: String = ByteArrayOutputStream().use { outputStream ->
+        project.exec {
+            executable("git")
+            args("log",  "$start..$end", "--format=format:%h %s")
+            standardOutput = outputStream
+        }
+
+        outputStream.toString()
+    }
+
+    return output.split("\n")
+}
+
+fun formatGitLog(commitLog: String): String {
+    val hash = commitLog.take(7)
+    val message = commitLog.substring(8) // Get message after commit hash + space between
+    return "[$hash](https://github.com/Crazy-Crew/${rootProject.name}/commit/$hash) $message<br>"
+}
+
+val changes = """
+${rootProject.file("CHANGELOG.md").readText(Charsets.UTF_8)} 
+## Commits  
+<details>  
+<summary>Other</summary>
+
+$commitLog
+</details>
+""".trimIndent()
+
 tasks {
     compileJava {
         options.encoding = Charsets.UTF_8.name()
@@ -42,6 +81,7 @@ tasks {
 
         exclude("META-INF/**")
     }
+
 }
 
 java {
